c37bc8ee96d9609df8c73bfc296e501304dcac8d
vperezlo <victor.perez-lopez-external@capgemini.com>
2022-07-04 13:18:09 +0200

    Restructure of the code
    Seems to work fine, requires further testing though

--- Project/Utils/preprocess.py
+++ Project/Utils/preprocess.py
@@ -2,8 +2,60 @@ import pandas as pd
 
 from Project.Utils.rename_value_column import rename_value_column
 
+indicators = {
+    
+    #databank
+    'CPIA gender equality rating (1=low to 6=high)': 'Gender Equality',
+    'Prevalence of undernourishment (% of population)': '% Undernourishment',
+    
+    #faostat
+    'Credit to Agriculture, Forestry and Fishing': 'CreditToAgriFishForest',
+    '2.a.1 Agriculture value added share of GDP (%)': 'AgriShareGDP',
+    'Employment by status of employment, total, rural areas': 'EmploymentRural',
+    'Gross Domestic Product': 'GDP',
+    'Share of employment in agriculture, forestry and fishing in total employment': '%EmploymentAgriFishForest',
+    'Agriculture': 'TotalAgri',
+    
+    #kaggle
+    'Gender Inequality': 'Gender Inequality',
+    
+    #ourworldindata
+    'Armed forces personnel (% of total labor force)': '% Soldiers', 
+    'Crude marriage rate (per 1,000 inhabitants)': 'Marriage Rate',
+    
+    #theworldbank  
+    'Birth rate, crude (per 1,000 people)': 'Birth Rate',
+    'Death rate, crude (per 1,000 people)': 'Death Rate',
+    'Intentional homicides (per 100,000 people)': 'Homicides',
+    'Life expectancy at birth, total (years)': 'Life Expectancy',
+    'Lifetime risk of maternal death (%)': 'Maternal Death Risk',
+    'Literacy rate, adult total (% of people ages 15 and above)': 'Literacy Rate',
+    'Mortality rate, infant (per 1,000 live births)': 'Infant Mortality',
+    'Population growth (annual %)': '% Population Growth',
+    'Rural population (% of total population)': '% Rural Population',
+    'Suicide mortality rate (per 100,000 population)': 'Suicide Rate',
+    
+    #UN_Data
+    'Value': 'Gini',
+    
+    #WID
+    #(To Do)
+    
+    #WorldInData
+    'civlib_vdem_owid': 'Civil Liberties',
+    'Employment-to-population ratio, men (%)': '% Men Employment',
+    'Employment-to-population ratio, women (%)': '% Women Employment',
+    'Population (historical estimates)': 'Population',
+    'freeexpr_vdem_owid': 'Freedom of Expression',
+    'Indicator:Domestic general government health expenditure (GGHE-D) as percentage of general government expenditure (GGE) (%)': '% Healthcare Investment',
+    'Industry as % of total employment -- ILO modelled estimates, May 2017': '% Employment Industry',
+    'UIS: Mean years of schooling of the population age 25+. Female': 'Women Schooling Years',
+    'UIS: Mean years of schooling of the population age 25+. Male': 'Men Schooling Years',
+    'Government expenditure on education, total (% of government expenditure)': '% Education Expenditure'
 
-def preprocess (dataframe, columns_index, indicators, columns_rename, year_range = (1990,2020),treatment = '', melt_on_value = None, rename_value_columns = False, inplace = False):
+}
+
+def preprocess (dataframe: pd.DataFrame, columns_index, columns_rename,*, year_range = (1990,2020), treatment = '', melt_on_value = None, rename_value_columns = False, inplace = False):
     
     """
         Take a dataframe, rearrange its columns or rows, rename them if needed, and return the resulting dataframe
@@ -68,7 +120,7 @@ def preprocess (dataframe, columns_index, indicators, columns_rename, year_range
     
     #Shorten indicators column name and remove all the other columns except for the index columns
     dataframe.rename(columns = indicators, inplace = True)
-    dataframe.drop(dataframe.columns.difference(columns_index + list(indicators.values())), axis = 1, inplace=True)
+    dataframe.drop(dataframe.columns.difference(list(columns_index) + list(indicators.values())), axis = 1, inplace=True)
     
     #Remove rows with no country
     dataframe.dropna(subset=column_country, inplace=True)
--- Project/main.py
+++ Project/main.py
@@ -2,6 +2,7 @@
 
 import os
 from Project.Utils.merge_data import merge_data
+from Project.Utils.preprocess import preprocess
 
 from Project.Utils.read_databases import read_databases
 
@@ -9,8 +10,17 @@ from Project.Utils.read_databases import read_databases
 read_path = os.getcwd() + '\Databases' #Path to your databases folder to be read
 write_path = os.getcwd() + '\Output' #Path to the folder you want to store the dataframes
 columns_index = ('Country', 'Year')
+indicators = {}
+columns_rename = {}
+year_range = range(1950, 2050)
 
-def main(self):
+def main():
     dict_dataframes = read_databases(read_path)
-    df = merge_data(dict_dataframes.values(), )
+    #for df in dict_dataframes.values():
+    #    preprocess(df, columns_index, indicators, columns_rename, year_range, inplace = True)
+    data_list = list(dict_dataframes.values())
+    df = merge_data(data_list, columns_index)
+    print(df)
 
+if __name__ == '__main__':
+    main()
