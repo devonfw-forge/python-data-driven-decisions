e76fae5819e5c72e95259bfadcccd9b4ebe3f665
smanolesCAPG <silviu-valentin.manolescu-external@capgemini.com>
2022-07-14 09:16:43 +0200

    SilverNotebook- ReorganizeCode and Preprocess
    SilverNotebook finished- It aims to reorganize the code and preprocess the database for irrelevant entries TASKS from Trello

--- Project/Utils/data_treat.py
+++ Project/Utils/data_treat.py
@@ -9,33 +9,35 @@ def iqr_treatment(data):
                 the dataframe to be modified 
     """
 
-    for columna in data.columns[2:]:
+    for columna in data.columns[2:] :
 
-        count = 0
-        outliers = []
-        auxOutliers = []
+        if len(data[columna].value_counts()) > 0:
+            count = 0
+            outliers = []
+            auxOutliers = []
 
-        # calculate interquartile range
-        q25, q75 = percentile(data[columna], 25), percentile(data[columna], 75)
-        iqr = q75 - q25
-        # calculate the outlier cutoff
-        cut_off = iqr * 1.5
-        lower, upper = q25 - cut_off, q75 + cut_off
-        # identify true outliers and replaces them with nan (if there are > 3 outliers  consecutive it's considered as a fake outlier)
-        for pos,x in enumerate(data[columna]):
-            outlierIndex = data[columna].index[pos]
-            if x < lower or x > upper:
-                auxOutliers.append(outlierIndex)
-                count += 1
-            else:
-                if count >=3:
-                    del auxOutliers[-count:]
-                count = 0
-        if count >=3:
-                    del auxOutliers[-count:]
-            
-        for elem in auxOutliers:
-            data.loc[elem, columna]= np.nan
+            # calculate interquartile range
+            q25, q75 = percentile(data[columna], 25), percentile(data[columna], 75)
+            iqr = q75 - q25
+            # calculate the outlier cutoff
+            cut_off = iqr * 1.5
+            lower, upper = q25 - cut_off, q75 + cut_off
+            # identify true outliers and replaces them with nan (if there are > 3 outliers  consecutive it's considered as a fake outlier)
+            for pos,x in enumerate(data[columna]):
+                outlierIndex = data[columna].index[pos]
+                if x < lower or x > upper:
+                    auxOutliers.append(outlierIndex)
+                    count += 1
+                else:
+                    if count >=3:
+                        del auxOutliers[-count:]
+                    count = 0
+            if count >=3:
+                        del auxOutliers[-count:]
+                
+            for elem in auxOutliers:
+                data.loc[elem, columna]= np.nan
+    
 
     
     return data
